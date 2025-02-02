3efc573ac54d6565a5d9b857c248b3d97d4a814f
Tobias Vidal <150728892+tobyvidal@users.noreply.github.com>
2025-02-02 20:18:34 +0100

    Add files via upload

---
+++ js/jsInstantes.js
@@ -0,0 +1,44 @@
+// Función para abrir el modal y mostrar la imagen y la descripción
+function openModal(imgSrc, desc) {
+    // Obtener el modal y los elementos de la imagen y la descripción
+    var modal = document.getElementById("modal");
+    var modalImg = document.getElementById("modal-img");
+    var modalDesc = document.getElementById("modal-desc");
+
+    // Asignar la imagen y la descripción al modal
+    modalImg.src = "imagenes/" + imgSrc + ".png"; // Asegúrate de que las rutas sean correctas
+    modalDesc.textContent = desc;
+
+    // Mostrar el modal
+    modal.style.display = "block";
+}
+
+function openModalViajes(imgSrc, desc) {
+    // Obtener el modal y los elementos de la imagen y la descripción
+    var modal = document.getElementById("modal");
+    var modalImg = document.getElementById("modal-img");
+    var modalDesc = document.getElementById("modal-desc");
+
+    // Asignar la imagen y la descripción al modal
+    modalImg.src = "../imagenesBenichatel/" + imgSrc + ".png"; // Asegúrate de que las rutas sean correctas
+    modalDesc.textContent = desc;
+
+    // Mostrar el modal
+    modal.style.display = "block";
+}
+
+
+
+// Función para cerrar el modal
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+// Cerrar el modal si se hace clic fuera del contenido del modal
+window.onclick = function(event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
